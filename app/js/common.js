2490f842cfa05f8c1a8f676438462f0cf0882703
Александр Морозов <frostmoroz83@yandex.ru>
2019-02-04 17:21:02 +0400

    04.02

--- app/js/common.js
+++ app/js/common.js
@@ -18,6 +18,30 @@ $(function() {
 				$('.hamburger').removeClass('is-active');
 				});
 
+				$("#my-menu").click(function() {
+					api.close();
+			});
+
+	$('.carousel-services').owlCarousel({
+		loop: true,
+		nav: true,
+		smartSpeed: 700,
+		navText: ['<i class="fa fa-angle-double-left"></i>', '<i class="fa fa-angle-double-right"></i>'],
+		responsiveClass: true,
+		responsive: {
+			0: {
+				items: 1
+			},
+			800: {
+				items: 2
+			},
+			1100: {
+				items: 3
+			}
+		}
+	});
+
+
 	
 	
 });
