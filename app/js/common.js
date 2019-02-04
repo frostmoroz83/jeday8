9333177903a0d77a1a85645015b84552c9c96d3a
Александр Морозов <frostmoroz83@yandex.ru>
2019-02-04 10:17:24 +0400

    цукцк

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,23 @@
 $(function() {
 
-	// Custom JS
+	$('#my-menu').mmenu({
+		extensions: [ 'theme-black', 'effect-menu-slide', 'pagedim-black', 'position-right'],
+		navbar: {
+			title: '<img src="img/logo-1.svg" alt="Салон красоты Смитрлер">'
+		},
+		offCanvas: {
+			
+		}
+	});
 
+	var api = $("#my-menu").data( "mmenu" );
+		api.bind( "open:finish", function( ) {
+			$('.hamburger').addClass('is-active');
+			});
+			api.bind( "close:finish", function( ) {
+				$('.hamburger').removeClass('is-active');
+				});
+
+	
+	
 });
