7188e7d31295434ea3bc7dbaa6a627121c56b863
Alexander Morozov <frostmoroz83@yandex.ru>
2019-02-27 12:14:02 +0400

    finish

--- app/js/common.js
+++ app/js/common.js
@@ -1,31 +1,31 @@
-$(function() {
+$(function () {
 
 	$('#my-menu').mmenu({
-		extensions: [ 'theme-black', 'effect-menu-slide', 'pagedim-black', 'position-right'],
+		extensions: ['widescreen', 'theme-black', 'effect-menu-slide', 'pagedim-black', 'position-right'],
 		navbar: {
 			title: '<img src="img/logo-1.svg" alt="Салон красоты Смитрлер">'
 		},
-		offCanvas: {
-		}
+		offCanvas: {}
 	});
 
-	var api = $("#my-menu").data( "mmenu" );
-		api.bind( "open:finish", function( ) {
-			$('.hamburger').addClass('is-active');
-			});
-			api.bind( "close:finish", function( ) {
-				$('.hamburger').removeClass('is-active');
-				});
-				$("#my-menu").click(function() {
-					api.close();
-			});
+	var api = $("#my-menu").data("mmenu");
+	
+	api.bind("open:finish", function () {
+		$('.hamburger').addClass('is-active');
+	});
+	api.bind("close:finish", function () {
+		$('.hamburger').removeClass('is-active');
+	});
+	$("#my-menu").click(function () {
+		api.close();
+	});
 
 
-			$('.carousel-services').on('initialized.owl.carousel', function() {
-				setTimeout(function() {
-					carouselService();
-				},100 );
-			});
+	$('.carousel-services').on('initialized.owl.carousel', function () {
+		setTimeout(function () {
+			carouselService();
+		}, 100);
+	});
 	$('.carousel-services').owlCarousel({
 		loop: true,
 		nav: true,
@@ -51,34 +51,80 @@ $(function() {
 	function carouselService() {
 		$('.carousel-services-item').each(function () {
 			var ths = $(this),
-					thsh = ths.find('.carousel-services-content').outerHeight();
-					ths.find('.carousel-services-image').css('min-height', thsh);
+				thsh = ths.find('.carousel-services-content').outerHeight();
+			ths.find('.carousel-services-image').css('min-height', thsh);
 		});
-		}carouselService();
-	
+	}
+	carouselService();
 
-	$('.carousel-services-composition .h3').each(function() {
+
+	$('.carousel-services-composition .h3').each(function () {
 		var ths = $(this);
 		ths.html(ths.html().replace(/(\S+)\s*$/, '<span>$1</span>'));
 	});
 
-	$('section .h2').each(function() {
+	$('section .h2').each(function () {
 		var ths = $(this);
 		ths.html(ths.html().replace(/^(\S+)/, '<span>$1</span>'));
 	});
 
-	$('select').selectize({	});
+	$('select').selectize({});
+
+
+	$('.reviews').owlCarousel({
+		loop: true,
+		items: 1,
+		smartSpeed: 700,
+		nav: false,
+		autoHeight: true
+	});
+
+	$('.partners').owlCarousel({
+		loop: true,
+		samrtSpeed: 700,
+		dors: false,
+		nav: true,
+		navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'],
+		responsiveClass: true,
+		responsive: {
+			0: {
+				items: 1
+			},
+			768: {
+				items: 2
+			},
+			992: {
+				items: 3
+			},
+			1200: {
+				items: 4
+			}
+		}
+	});
+
+	$(window).scroll(function () {
+		if ($(this).scrollTop() > $(this).height()) {
+			$('.top').addClass('active');
+	} else {
+		$('.top').removeClass('active');
+	}
+	});
+
+	$('.top').click(function() {
+		$('html, body').stop().animate({scrollTop: 0}, 'slow', 'swing');
+	});
+
 
 	//E-mail Ajax Send
-	$("form.callback").submit(function() { //Change
+	$("form.callback").submit(function () { //Change
 		var th = $(this);
 		$.ajax({
 			type: "POST",
 			url: "mail.php", //Change
 			data: th.serialize()
-		}).done(function() {
+		}).done(function () {
 			$(th).find('.success').addClass('active').css('display', 'flex').hide().fadeIn();
-			setTimeout(function() {
+			setTimeout(function () {
 				// Done Functions
 				$(th).find('.success').removeClass('active').fadeOut();
 				th.trigger("reset");
@@ -91,10 +137,15 @@ $(function() {
 	//Resize Window
 	function onResize() {
 		$('.carousel-services-content').equalHeights();
-	}onResize();
-	window.onresize = function() {
+	}
+	onResize();
+	window.onresize = function () {
 		onResize();
 	};
-	
-	
+
+
 });
+
+$(window).on('load', function() {
+	$('.preloader').delay(1000).fadeOut('slow');
+})
